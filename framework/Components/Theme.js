9a9cf2ce4e8628c5182190bd45796387a8d04b6e
Miracle Obafemi <miracleobafemi09@gmail.com>
2024-03-28 18:00:10 +0100

    New files added

--- framework/Components/Theme.js
+++ framework/Components/Theme.js
@@ -5,5 +5,17 @@ export const Theme = {
         purple300: "#E1AFD1",
         purple500: "#AD88C6",
         purple700: "#7469B6",
+    },
+    font: {
+        brand: "Pacifico_400Regular",
+        text100: "Raleway_100Thin",
+        text200: "Raleway_200ExtraLight",
+        text300: "Raleway_300Light",
+        text400: "Raleway_400Regular",
+        text500: "Raleway_500Medium",
+        text600: "Raleway_600SemiBold",
+        text700: "Raleway_700Bold",
+        text800: "Raleway_800ExtraBold",
+        text900: "Raleway_900Black",
     }
 }
---
+++ framework/Screens/LogIn.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export function LogIn() {
+    return (
+        <View>
+            <Text>LogIn</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
