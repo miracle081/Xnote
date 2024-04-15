711e663e8822b25a9a15d15bfdeae35fdbd535c2
Miracle Obafemi <miracleobafemi09@gmail.com>
2024-04-15 17:52:57 +0100

    Note book

--- framework/Service/APIkeys.key.js
+++ framework/Service/APIkeys.key.js
@@ -2,10 +2,11 @@
 export const PAYSTACK_PUBLIC_KEY = "pk_test_92fcc0077ec7f42a73ff01c87db79c3698b06dec"
 
 export const firebaseAPI = {
-    apiKey: "AIzaSyDeHdRptp7SvTwVbdQbULeV4rCoRmWAnK4",
-    authDomain: "jobplug-e45ae.firebaseapp.com",
-    projectId: "jobplug-e45ae",
-    storageBucket: "jobplug-e45ae.appspot.com",
-    messagingSenderId: "771097398518",
-    appId: "1:771097398518:web:b78367d98abb53a322b0d7"
+    apiKey: "AIzaSyBpuHeiWgqh1RIx8Io0EFUB8qVfMUqZ41s",
+    authDomain: "x-note-5fd32.firebaseapp.com",
+    projectId: "x-note-5fd32",
+    storageBucket: "x-note-5fd32.appspot.com",
+    messagingSenderId: "220127384688",
+    appId: "1:220127384688:web:f92c5814eba1d18c1050b5",
+    appId: "1:220127384688:web:f92c5814eba1d18c1050b5",
 }
